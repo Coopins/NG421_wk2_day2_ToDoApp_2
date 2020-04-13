9ef6db7489848da7c6b97e15ab8a43d829578bfd
Chris <coopertime08@gmail.com>
2020-04-12 21:04:58 -0500

    Modal Attempting to Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,42 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Todos';
   todoList: any [] = [];
   todoTitle: string;
-  todoId: number = 0;
+  todoId = 0;
+
+  constructor(private modaldService: NgbModal) {
+
+  }
   ngOnInit() {
     this.todoTitle = '';
     this.todoList = [
       // example of how to make an item in todo list
       { title: 'Install Angular CLI', isDone: false },
-    
+
     ];
   }
-  addTodo():void {
+  addTodo(): void {
     this.todoList.push({
       title: this.todoTitle,
       isDone: false
     });
-    
+
     // resets our todoTitle variable to an empty string
     this.todoTitle = '';
   }
-  deleteTodo(todo:any) {
+  deleteTodo(todo: any) {
+// show
+this.modaldService.open(ConfirmationModalComponent);
+// if the result is yes
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
